a3be2ea70f531a15e8dd664532b975893669258d
An Vu Trong <vta.bsn@gmail.com>
2024-12-05 16:24:29 +0700

    Add Routes

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,5 +1,5 @@
 import Directory from '../../components/directory/directory.component';
-
+import {Outlet} from 'react-router-dom';
 
 const Home = () => {
   const categories = [
@@ -31,7 +31,10 @@ const Home = () => {
   ];
 
   return (
-      <Directory categories={categories} />    
+    <div>
+        <Directory categories={categories} />    
+        <Outlet/>
+    </div>
   );
 };
 
