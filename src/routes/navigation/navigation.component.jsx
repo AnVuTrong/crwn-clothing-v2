3c01c55110df743624a8bbb5e288a4e204c6f9bc
An Vu Trong <vta.bsn@gmail.com>
2024-12-05 17:43:18 +0700

    Add Logo and Box-sizing

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,17 +1,20 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
+
 const Navigation = () => {
 	return (
 		<Fragment>
 			<div className="navigation">
 				{/* Logo, also a link to the home page */}
 				<Link className="logo-container" to="/">
-					<div>Logo</div>
+					<CrwnLogo className="logo" />
 				</Link>
 
 				{/* Links container */}
-				<div className="links-container">
+				<div className="nav-links-container">
 					<Link className="nav-link" to="/shop">
 						SHOP
 					</Link>
