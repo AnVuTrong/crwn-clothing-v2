14e849b2406efd2d6a71e4595fbe135cdddc77bc
An Vu Trong <vta.bsn@gmail.com>
2024-12-05 16:36:34 +0700

    Add functional pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import Home from './routes/home/home.component';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet} from 'react-router-dom';
 
+import Home from './routes/home/home.component';
+
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>Menu Item</h1>
+      </div>
+      <Outlet/>
+    </div>
+  )
+};
 
 const Shop = () => {
   return <h1>User now in shop</h1>
@@ -9,7 +21,8 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home/>}>
+      <Route path='/' element={<Navigation/>}>
+        <Route index={true} element={<Home/>}/>
         <Route path='shop' element={<Shop/>}/>
       </Route>
     </Routes>
